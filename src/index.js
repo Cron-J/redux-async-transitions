6ec47eb058e5019e3946b3d2281e390cc59f8fbd
sureshvarman-cronj <suresh.varman@cronj.com>
2017-10-06 01:11:18 +0530

    with test cases

--- src/index.js
+++ src/index.js
@@ -5,183 +5,219 @@ exports.__esModule = true;
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var isPromise = function isPromise(obj) {
-  return obj && typeof obj.then === 'function';
+    return obj && typeof obj.then === 'function';
 };
 var hasPromiseProps = function hasPromiseProps(obj) {
-  return Object.keys(obj).some(function (key) {
-    return isPromise(obj[key]);
-  });
+    return Object.keys(obj).some(function (key) {
+        return isPromise(obj[key]);
+    });
 };
 
 var resolveProps = function resolveProps(obj) {
-  var props = Object.keys(obj);
-  var values = props.map(function (prop) {
-    return obj[prop];
-  });
+    var props = Object.keys(obj);
+    var values = props.map(function (prop) {
+        return obj[prop];
+    });
 
-  return Promise.all(values).then(function (resolvedArray) {
-    return props.reduce(function (acc, prop, index) {
-      acc[prop] = resolvedArray[index];
-      return acc;
-    }, {});
-  });
+    return Promise.all(values).then(function (resolvedArray) {
+        return props.reduce(function (acc, prop, index) {
+            acc[prop] = resolvedArray[index];
+            return acc;
+        }, {});
+    });
 };
 
 var getNonPromiseProperties = function getNonPromiseProperties(obj) {
-  return Object.keys(obj).filter(function (key) {
-    return !isPromise(obj[key]);
-  }).reduce(function (acc, key) {
-    acc[key] = obj[key];
-    return acc;
-  }, {});
+    return Object.keys(obj).filter(function (key) {
+        return !isPromise(obj[key]);
+    }).reduce(function (acc, key) {
+        acc[key] = obj[key];
+        return acc;
+    }, {});
 };
 
 var getActionObjects = function getActionObjects(obj) {
-  var actionObject = {};
-  for (var key in obj) {
-    if (key != 'payload' && key != 'type' && key != 'types') {
-      actionObject[key] = obj[key];
+    var actionObject = {};
+    for (var key in obj) {
+        if (key != 'payload' && key != 'type' && key != 'types') {
+            actionObject[key] = obj[key];
+        }
     }
-  }
-  return actionObject;
+    return actionObject;
 };
 
 var dispatcher = function dispatcher(action, store, history) {
-  var types = action.types;
-  var payload = action.payload;
-  var meta = action.meta;
-  if (types) {
-    var onSuccess = null;
-    var onFail = null;
-    var onPending = null;
-
-    var metaFunction = meta && meta.transition ? meta.transition(store.getState(), action) : null;
-    onSuccess = metaFunction && metaFunction.onSuccess ? metaFunction.onSuccess : null;
-    onFail = metaFunction && metaFunction.onFail ? metaFunction.onFail : null;
-    onPending = metaFunction && metaFunction.onPending ? metaFunction.onPending : null;
-
-    if (!types || !hasPromiseProps(action.payload)) {
-      return store.dispatch(action);
-    }
-
-    if (onPending) {
-      var onPendingData = onPending();
-      var func = onPendingData.func;
-      var path = onPendingData.path;
-
-      if (func) {}
-        dispatcher(func(), store, history);
-
-      if (path) {
-        var query = onPendingData.query;
-        var replace = onPendingData.replace;
-
-        var method = replace ? 'replace' : 'push';
-        history[method]({pathname: path, query: query});
-      }
-    }
-
-    var actionProperties = getActionObjects(action);
-    var nonPromiseProperties = getNonPromiseProperties(payload);
-
-    var PENDING = types[0];
-    var RESOLVED = types[1];
-    var REJECTED = types[2];
-
-    actionProperties.type = PENDING;
-    actionProperties.payload = _extends({}, nonPromiseProperties);
-    store.dispatch(actionProperties);
-    return resolveProps(payload).then(function (results) {
-      actionProperties.type = RESOLVED;
-      actionProperties.payload = _extends({}, results);
-      if (onSuccess) {
-        store.dispatch(actionProperties);
-        var successData = onSuccess(results);
-        if (successData) {
-          var path = successData.path;
-          var func = successData.func;
-
-          if (func)
-            dispatcher(func(), store, history);
-
-          if (path) {
-            var query = successData.query;
-            var replace = successData.replace;
-
-            var method = replace ? 'replace' : 'push';
-            history[method]({pathname: path, query: query});
-          }
-        }
-      } else {
-        store.dispatch(actionProperties);
-      }
-    }, function (error) {
-      actionProperties.type = REJECTED;
-      actionProperties.error = true;
-      actionProperties.payload = _extends({}, error);
-      if (onFail) {
-        store.dispatch(actionProperties);
-        var failData = onFail(error);
-        if (failData) {
-          var path = failData.path;
-          var func = failData.func;
-
-          if (func)
-            dispatcher(func(), store, history);
-
-          if (path) {
-            var query = failData.query;
-            var replace = failData.replace;
-
-            var method = replace ? 'replace' : 'push';
-
-            history[method]({pathname: path, query: query});
-          }
-        }
-      } else {
-        return store.dispatch(actionProperties);
-      }
-    });
-  } else if (action.type) {
-    var type = action.type;
+    var types = action.types;
+    var payload = action.payload;
     var meta = action.meta;
+    if (types) {
+        var onSuccess = null;
+        var onFail = null;
+        var onPending = null;
 
-    store.dispatch(action);
+        var metaFunction = meta && meta.transition ? meta.transition(store.getState(), action) : null;
+        onSuccess = metaFunction && metaFunction.onSuccess ? metaFunction.onSuccess : null;
+        onFail = metaFunction && metaFunction.onFail ? metaFunction.onFail : null;
+        onPending = metaFunction && metaFunction.onPending ? metaFunction.onPending : null;
 
-    var transitionData = meta && meta.transition ? meta.transition(store.getState(), action) : null;
+        if (!types || !hasPromiseProps(action.payload)) {
+            return store.dispatch(action);
+        }
 
-    if (transitionData) {
-      var path = transitionData.path;
-      var func = transitionData.func;
+        if (onPending) {
+            var onPendingData = onPending();
+            var func = onPendingData.func;
+            var path = onPendingData.path;
 
-      if (func)
-        dispatcher(func(), store, history);
+            if (func)
+            dispatcher(func(), store, history);
 
-      if (path) {
-        var query = transitionData.query;
-        var replace = transitionData.replace;
+            if (path) {
+                var query = onPendingData.query;
+                var replace = onPendingData.replace;
+                var search = '';
 
-        var method = replace ? 'replace' : 'push';
+                for (var key in query) {
+                    if (!search) {
+                        search = '?' + key + '=' + query[key];
+                    } else {
+                        search += '&' + key + '=' + query[key];
+                    }
+                }
 
-        history[method]({pathname: path, query: query});
-      }
+                var method = replace ? 'replace' : 'push';
+                history[method]({pathname: path, search: search});
+            }
+        }
+
+        var actionProperties = getActionObjects(action);
+        var nonPromiseProperties = getNonPromiseProperties(payload);
+
+        var PENDING = types[0];
+        var RESOLVED = types[1];
+        var REJECTED = types[2];
+
+        actionProperties.type = PENDING;
+        actionProperties.payload = _extends({}, nonPromiseProperties);
+        store.dispatch(actionProperties);
+        return resolveProps(payload).then(function (results) {
+            actionProperties.type = RESOLVED;
+            actionProperties.payload = _extends({}, results);
+            if (onSuccess) {
+                store.dispatch(actionProperties);
+                var successData = onSuccess(results);
+                if (successData) {
+                    var path = successData.path;
+                    var func = successData.func;
+
+                    if (func)
+                    dispatcher(func(), store, history);
+
+                    if (path) {
+                        var query = successData.query;
+                        var replace = successData.replace;
+                        var search = '';
+
+                        for (var key in query) {
+                            if (!search) {
+                                search = '?' + key + '=' + query[key];
+                            } else {
+                                search += '&' + key + '=' + query[key];
+                            }
+                        }
+
+                        var method = replace ? 'replace' : 'push';
+                        history[method]({pathname: path, search: search});
+                    }
+                }
+            } else {
+                store.dispatch(actionProperties);
+            }
+        }, function (error) {
+            actionProperties.type = REJECTED;
+            actionProperties.error = true;
+            actionProperties.payload = _extends({}, error);
+            if (onFail) {
+                store.dispatch(actionProperties);
+                var failData = onFail(error);
+                if (failData) {
+                    var path = failData.path;
+                    var func = failData.func;
+
+                    if (func)
+                    dispatcher(func(), store, history);
+
+                    if (path) {
+                        var query = failData.query;
+                        var replace = failData.replace;
+                        var search = '';
+
+                        for (var key in query) {
+                            if (!search) {
+                                search = '?' + key + '=' + query[key];
+                            } else {
+                                search += '&' + key + '=' + query[key];
+                            }
+                        }
+
+                        var method = replace ? 'replace' : 'push';
+                        history[method]({pathname: path, search: search});
+                    }
+                }
+            } else {
+                return store.dispatch(actionProperties);
+            }
+        });
+    } else if (action.type) {
+        var type = action.type;
+        var meta = action.meta;
+
+        store.dispatch(action);
+
+        var transitionData = meta && meta.transition ? meta.transition(store.getState(), action) : null;
+
+        if (transitionData) {
+            var path = transitionData.path;
+            var func = transitionData.func;
+
+            if (func)
+                dispatcher(func(), store, history);
+
+            if (path) {
+                var query = transitionData.query;
+                var replace = transitionData.replace;
+                var search = '';
+
+                for (var key in query) {
+                    if (!search) {
+                        search = '?' + key + '=' + query[key];
+                    } else {
+                        search += '&' + key + '=' + query[key];
+                    }
+                }
+
+                var method = replace ? 'replace' : 'push';
+
+                history[method]({pathname: path, search: search});
+            }
+        }
+
+        return action;
     }
-
-    return action;
-  }
 }
 
+
 exports['default'] = function(history) {
-  return function (next) {
-    return function (reducer, initialState) {
-      var store = next(reducer, initialState);
-      return _extends({}, store, {
-        dispatch: function dispatch(action) {
-          dispatcher(action, store, history);
+    return function (next) {
+        return function (reducer, initialState) {
+            var store = next(reducer, initialState);
+            return _extends({}, store, {
+                dispatch: function dispatch(action) {
+                    dispatcher(action, store, history);
+                }
+            });
         }
-      });
     }
-  }
 }
 
 module.exports = exports['default'];
---
+++ test/index.js
@@ -0,0 +1,366 @@
+import expect from 'expect';
+import Promise from 'bluebird';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createReducer } from 'redux-create-reducer';
+import createHistory from 'history/createMemoryHistory';
+
+import asyncTransitionMiddleware from '../src';
+
+const history = createHistory();
+
+const getStore = (reducers, initialState) =>
+{
+    let createStoreWithMiddleware = compose(asyncTransitionMiddleware(history))(createStore);
+    const store = createStoreWithMiddleware(reducers, initialState);
+    return store;
+};
+
+describe('redux-async-transitions', () =>
+{
+    // on synchronized calls
+    describe('Sync', () =>
+    {
+        // on Normal store listener case
+        it('Call', (done) =>
+        {
+            const data = {test: "test-normal-case"};
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_HAPPENED"](state, action) {
+                    return {
+                        test: action.payload.test
+                    }
+                }
+            });
+
+            const store = getStore(reducer, {});
+            store.subscribe(() =>
+            {
+                expect(store.getState()).toEqual(data);
+                done();
+            });
+            store.dispatch({type: 'SOMETHING_HAPPENED', payload: data});
+        });
+    });
+
+    // on Asynchronized calls
+    describe('Async', () =>
+    {
+
+        // on Async calls Failure
+        it('onPending', (done) =>
+        {
+            const data = {test: 'Initial test data'};
+            const successData = {test: 'Result is recieved'};
+
+            const samplePromise = () =>
+            {
+                return new Promise((resolve, reject) =>
+                {
+                    setTimeout(() =>
+                    {
+                        resolve(successData);
+                    });
+                });
+            }
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_HAPPENED"](state, action) {
+                    expect(action.payload.test).toEqual(data.test);
+                    done();
+                }
+            });
+
+            const store = getStore(reducer, {});
+
+            store.dispatch({types: ['SOMETHING_HAPPENED', 'SOMETHING_RESOVLED', 'SOMETHING_REJECTED'], payload: {test: data.test, response: samplePromise() }});
+        });
+
+        // on Asynchronized calls success
+        it('OnSuccess', (done) =>
+        {
+            const data = {test: "Initial test data"};
+            const successData = {test: "Result is recieved"};
+
+            const samplePromise = () =>
+            {
+                return new Promise((resolve, reject) =>
+                {
+                    setTimeout(() =>
+                    {
+                        resolve(successData);
+                    });
+                });
+            }
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_RESOVLED"](state, action) {
+                    expect(action.payload.response).toEqual(successData);
+                    done();
+                }
+            });
+
+            const store = getStore(reducer, {});
+
+            store.dispatch({types: ['SOMETHING_HAPPENED', 'SOMETHING_RESOVLED', 'SOMETHING_REJECTED'], payload: {response: samplePromise() }});
+        });
+
+        // on Async calls Failure
+        it('onFail', (done) =>
+        {
+            const data = {test: 'Initial test data'};
+            const failureData = {test: 'Result is not recieved'};
+
+            const samplePromise = () =>
+            {
+                return new Promise((resolve, reject) =>
+                {
+                    setTimeout(() =>
+                    {
+                        reject(failureData);
+                    });
+                });
+            }
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_REJECTED"](state, action) {
+                    expect(action.payload).toEqual(failureData);
+                    done();
+                }
+            });
+
+            const store = getStore(reducer, {});
+
+            store.dispatch({types: ['SOMETHING_HAPPENED', 'SOMETHING_RESOVLED', 'SOMETHING_REJECTED'], payload: {response: samplePromise() }});
+        });
+    });
+
+    // sync with transition
+    describe('Sync with transition', () =>
+    {
+        // on path change
+        it('PathChange', (done) =>
+        {
+            const stateData = {test: "test1"};
+            const failurePath = '/some/path';
+
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_HAPPENED"](state, action) {
+                    expect(action.payload).toEqual(stateData);
+                }
+            });
+
+            const unlisten = history.listen((location, action) => {
+                expect(location.pathname).toEqual(failurePath);
+                unlisten();
+                done();
+            })
+
+            const store = getStore(reducer, {});
+
+            store.dispatch({
+                type: 'SOMETHING_HAPPENED',
+                payload: stateData,
+                meta: {
+                    transition: () => ({
+                        path: failurePath
+                    })
+                }
+            });
+        });
+
+        // on path change with query
+        it('pathChange with Query', (done) =>
+        {
+            const stateData = {test: "test1"};
+            const transitionPath = '/some/path';
+            const query = {someKey: 'someValue', someTest: 'someTest1'};
+
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_HAPPENED"](state, action) {
+                    expect(action.payload).toEqual(stateData);
+                }
+            });
+
+            const unlisten = history.listen((location, action) => {
+                expect(location.search).toEqual('?someKey=someValue&someTest=someTest1');
+                expect(location.pathname).toEqual(transitionPath);
+                unlisten();
+                done();
+            });
+
+            const store = getStore(reducer, {});
+
+            store.dispatch({
+                type: 'SOMETHING_HAPPENED',
+                payload: stateData,
+                meta: {
+                    transition: () => ({
+                        path: transitionPath,
+                        query: query
+                    })
+                }
+            });
+        });
+
+        // on path change with action
+        it('pathChange with Action', (done) =>
+        {
+            const stateData = {test: "test1"};
+            const subStateData = {test: "test2"};
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_HAPPENED"](state, action) {
+                    expect(action.payload).toEqual(stateData);
+                    return {
+                        test: action.payload.test
+                    }
+                },
+                ["SOMETHING_SUB_CALL"](state, action) {
+                    expect(action.payload).toEqual(subStateData);
+                    done();
+                }
+            });
+
+            const store = getStore(reducer, {});
+
+            store.dispatch({
+                type: 'SOMETHING_HAPPENED',
+                payload: stateData,
+                meta: {
+                    transition: () => ({
+                        func: () => {
+                            return {
+                                type: 'SOMETHING_SUB_CALL',
+                                payload: subStateData
+                            }
+                        }
+                    })
+                }
+            });
+        });
+
+        // on path change with action and path, query
+        it('pathChange overall', (done) =>
+        {
+            const stateData = {test: "test1"};
+            const subStateData = {test: "test2"};
+            const transitionPath = '/some/path';
+            const query = {someKey: 'someValue', someTest: 'someTest1'};
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_HAPPENED"](state, action) {
+                    expect(action.payload).toEqual(stateData);
+                    return {
+                        test: action.payload.test
+                    }
+                },
+                ["SOMETHING_SUB_CALL"](state, action) {
+                    expect(action.payload).toEqual(subStateData);
+                    return {
+                        test: action.payload.test
+                    }
+                }
+            });
+
+            const unlisten = history.listen((location, action) => {
+                expect(location.search).toEqual('?someKey=someValue&someTest=someTest1');
+                expect(location.pathname).toEqual(transitionPath);
+                unlisten();
+                done();
+            });
+
+            const store = getStore(reducer, {});
+
+            store.dispatch({
+                type: 'SOMETHING_HAPPENED',
+                payload: stateData,
+                meta: {
+                    transition: () => ({
+                        func: () => {
+                            return {
+                                type: 'SOMETHING_SUB_CALL',
+                                payload: subStateData
+                            };
+                        },
+                        path: transitionPath,
+                        query: query
+                    })
+                }
+            });
+        });
+    });
+
+    // Async with transition
+    describe('Async with transition', () =>
+    {
+        // async with onPending transition
+        it('Async with onPending transition', (done) =>
+        {
+            const data = {test: 'test1'};
+            const successData = {test: 'Result is recieved'};
+            const transitionPath = '/some/path';
+            const query = {someKey: 'someValue', someTest: 'someTest1'};
+
+            const samplePromise = () =>
+            {
+                return new Promise((resolve, reject) =>
+                {
+                    setTimeout(() =>
+                    {
+                        resolve(successData);
+                    });
+                });
+            }
+
+            let reducer = createReducer({}, {
+                ["SOMETHING_HAPPENED"](state, action) {
+                    expect(action.payload).toEqual(data);
+                    return {
+                        test: action.payload.test
+                    }
+                }
+            });
+
+
+            const unlisten = history.listen((location, action) => {
+                expect(location.search).toEqual('?someKey=someValue&someTest=someTest1');
+                expect(location.pathname).toEqual(transitionPath);
+                unlisten();
+                done();
+            });
+
+            const store = getStore(reducer, {});
+
+            store.dispatch({
+                types: ['SOMETHING_HAPPENED', 'SOMETHING_RESOVLED', 'SOMETHING_REJECTED'],
+                payload: {
+                    test: data.test,
+                    response: samplePromise()
+                },
+                meta: {
+                    transition: () => ({
+                        onPending: () => ({
+                            path : transitionPath,
+                            query: query
+                        })
+                    })
+                }
+            });
+        });
+
+        // async with onSuccess transition
+        it('Async with onSuccess transition', (done) =>
+        {
+            done();
+        });
+
+        // async with onFail transition
+        it('Async with onFail transition', (done) =>
+        {
+            done();
+        });
+    });
+});
